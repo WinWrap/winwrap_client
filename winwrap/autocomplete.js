5c0707c120e6c9b62459ae9b21a1f4b28e278ba3
EdwBennett <edw.bennett@gmail.comx>
2018-01-07 10:50:05 -0600

    move re_auto to constructor

--- winwrap/autocomplete.js
+++ winwrap/autocomplete.js
@@ -5,26 +5,7 @@
     class AutoComplete {
         constructor() {
             this.autoTypes_ = autotypes;
-            /*this.autoTypes_ = [ // xxx match documentation, MonacoTypes or such
-                "Method", // MC_METHOD
-                "Property", // MC_PROPERTY
-                "Property", // MC_DEFAULTPROPERTY
-                "Reference", // MC_EVENT
-                "Value", // MC_CONSTANT
-                "Enum", // MC_ENUM
-                "Unit", // MC_BUILTINTYPE
-                "Class", // MC_CLASS
-                "Module", // MC_MODULE
-                "Color", // MC_RECORD
-                "Interface", // MC_INTERFACE
-                "Function", // MC_DELEGATE
-                "Color", // MC_PREDECLAREDID
-                "Color", // MC_LIBRARY
-                "Reference", // MC_NAMESPACE
-                "Snippet", // MC_SPECIAL
-                "Color", // MC_CERTKEY
-                "Color"  // MC_EXTEND
-            ];*/
+            let this.re_auto = new RegExp(/(\s|^\'#|\.)$/); // Of\s
         }
         createDependencyProposals(autoComplete) {
             let deps = [];
@@ -45,12 +26,12 @@
                 triggerCharacters: [' ', '.', '#', '=', ',', '\t', '\xA0'],
                 provideCompletionItems: async function (model, position) {
                     let textUntilPosition = ww.EditorCode.textUntilPosition(model, position);
-                    //let match = textUntilPosition.match(/As\s\w+|\w\.\w+|\=\s\w+$/); // = CallersLine (global)
-                    //let match = textUntilPosition.match(/(\s|Imports\s|As\s|^\'#|\.)$/); // Of\s
-                    let re_auto = new RegExp(/(\s|^\'#|\.)$/);
-                    let match = textUntilPosition.match(re_auto);
+                    //xlet match = textUntilPosition.match(/As\s\w+|\w\.\w+|\=\s\w+$/); // = CallersLine (global)
+                    //xlet match = textUntilPosition.match(/(\s|Imports\s|As\s|^\'#|\.)$/); // Of\s
+                    //let re_auto = new RegExp(/(\s|^\'#|\.)$/); // Of\s
+                    let match = textUntilPosition.match(this.re_auto);
                     //let match = textUntilPosition.match(/(\s|^\'#|\.)$/); // Of\s
-                    //match = true;
+                    //xmatch = true;
                     if (match) {
                         await ww.AutoAuto.SendAsync(model, position);
                         return ww.AutoComplete.createDependencyProposals(ww.AutoAuto.Response.members); // incomplete not used
@@ -60,6 +41,7 @@
         }
     }
 
+    // no vaiables in class
     let autotypes = [ // xxx match documentation, MonacoTypes or such
             "Method", // MC_METHOD
             "Property", // MC_PROPERTY
