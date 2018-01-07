ef5934b192a93de809c032229926700a3adfb49a
EdwBennett <edw.bennett@gmail.comx>
2018-01-07 10:59:54 -0600

    put re_auto logic early in class

--- winwrap/autocomplete.js
+++ winwrap/autocomplete.js
@@ -5,7 +5,22 @@
     class AutoComplete {
         constructor() {
             this.autoTypes_ = autotypes;
-            let this.re_auto = new RegExp(/(\s|^\'#|\.)$/); // Of\s
+            //xlet match = textUntilPosition.match(/As\s\w+|\w\.\w+|\=\s\w+$/); // = CallersLine (global)
+            //xlet match = textUntilPosition.match(/(\s|Imports\s|As\s|^\'#|\.)$/); // Of\s
+            this.re_auto = new RegExp(/(\s|^\'#|\.)$/); // Of\s
+        }
+        Register() {
+            monaco.languages.registerCompletionItemProvider('vb', {
+                triggerCharacters: [' ', '.', '#', '=', ',', '\t', '\xA0'], // '(', ')'
+                provideCompletionItems: async function (model, position) {
+                    let textUntilPosition = ww.EditorCode.textUntilPosition(model, position);
+                    let match = textUntilPosition.match(this.re_auto); // limits traffic to server
+                    if (match) {
+                        await ww.AutoAuto.SendAsync(model, position);
+                        return ww.AutoComplete.createDependencyProposals(ww.AutoAuto.Response.members); // incomplete not used
+                    }
+                }
+            });
         }
         createDependencyProposals(autoComplete) {
             let deps = [];
@@ -20,48 +35,29 @@
             }
             return { isIncomplete: false, items: deps };
         }
-        Register() {
-            monaco.languages.registerCompletionItemProvider('vb', {
-                //triggerCharacters: [' ', '.', '#', '(', ')', '=', ',', '\t', '\xA0'],
-                triggerCharacters: [' ', '.', '#', '=', ',', '\t', '\xA0'],
-                provideCompletionItems: async function (model, position) {
-                    let textUntilPosition = ww.EditorCode.textUntilPosition(model, position);
-                    //xlet match = textUntilPosition.match(/As\s\w+|\w\.\w+|\=\s\w+$/); // = CallersLine (global)
-                    //xlet match = textUntilPosition.match(/(\s|Imports\s|As\s|^\'#|\.)$/); // Of\s
-                    //let re_auto = new RegExp(/(\s|^\'#|\.)$/); // Of\s
-                    let match = textUntilPosition.match(this.re_auto);
-                    //let match = textUntilPosition.match(/(\s|^\'#|\.)$/); // Of\s
-                    //xmatch = true;
-                    if (match) {
-                        await ww.AutoAuto.SendAsync(model, position);
-                        return ww.AutoComplete.createDependencyProposals(ww.AutoAuto.Response.members); // incomplete not used
-                    }
-                }
-            });
-        }
     }
 
     // no vaiables in class
     let autotypes = [ // xxx match documentation, MonacoTypes or such
-            "Method", // MC_METHOD
-            "Property", // MC_PROPERTY
-            "Property", // MC_DEFAULTPROPERTY
-            "Reference", // MC_EVENT
-            "Value", // MC_CONSTANT
-            "Enum", // MC_ENUM
-            "Unit", // MC_BUILTINTYPE
-            "Class", // MC_CLASS
-            "Module", // MC_MODULE
-            "Color", // MC_RECORD
-            "Interface", // MC_INTERFACE
-            "Function", // MC_DELEGATE
-            "Color", // MC_PREDECLAREDID
-            "Color", // MC_LIBRARY
-            "Reference", // MC_NAMESPACE
-            "Snippet", // MC_SPECIAL
-            "Color", // MC_CERTKEY
-            "Color"  // MC_EXTEND
-        ];
+        "Method", // MC_METHOD
+        "Property", // MC_PROPERTY
+        "Property", // MC_DEFAULTPROPERTY
+        "Reference", // MC_EVENT
+        "Value", // MC_CONSTANT
+        "Enum", // MC_ENUM
+        "Unit", // MC_BUILTINTYPE
+        "Class", // MC_CLASS
+        "Module", // MC_MODULE
+        "Color", // MC_RECORD
+        "Interface", // MC_INTERFACE
+        "Function", // MC_DELEGATE
+        "Color", // MC_PREDECLAREDID
+        "Color", // MC_LIBRARY
+        "Reference", // MC_NAMESPACE
+        "Snippet", // MC_SPECIAL
+        "Color", // MC_CERTKEY
+        "Color"  // MC_EXTEND
+    ];
 
     ww.AutoComplete = new AutoComplete();
 
