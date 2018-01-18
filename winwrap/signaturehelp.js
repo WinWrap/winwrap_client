b1ebd4ea1c2c8d73ea60f91aefa9388fa11fd9f4
EdwBennett <edw.bennett@gmail.comx>
2018-01-18 15:47:54 -0600

    add textUntilPostion [,] match

--- winwrap/signaturehelp.js
+++ winwrap/signaturehelp.js
@@ -12,7 +12,7 @@
                     // 1/15/18 - Tom
                     // added ' '
                     //let match = textUntilPosition.match('[( ]'); // was '('
-                    let match = textUntilPosition.match(/[( ]$/); // was '('
+                    let match = textUntilPosition.match(/[( ,]$/); // was '('
                     if (match) { // was [{}]
                         let response = await autoauto.SendAsync(model, position);
                         return signatureHelp._createSignatureHelp(response);
