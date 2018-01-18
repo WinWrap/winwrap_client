e10eac4a8f5f493ba41c6a52c29a8d46fd585fcb
EdwBennett <edw.bennett@gmail.comx>
2018-01-18 09:18:44 -0600

    fixed lint errors

--- winwrap/index.js
+++ winwrap/index.js
@@ -10,10 +10,10 @@ $(function () {
 
         let serverip = getSearchParams('serverip');
         let factory = {
-            'ww-remote': (basic, name) => { return new ww.Remote(basic, name, serverip) },
-            'ww-channel': (remote, name) => { return new ww.Channel(remote, name) },
-            'ww-ui': (channel, name) => { return new ww.UI(channel, name) },
-            'ww-item': (ui, element, name) => { return ww.CreateItem(ui, element, name) }
+            'ww-remote': (basic, name) => { return new ww.Remote(basic, name, serverip); },
+            'ww-channel': (remote, name) => { return new ww.Channel(remote, name); },
+            'ww-ui': (channel, name) => { return new ww.UI(channel, name); },
+            'ww-item': (ui, element, name) => { return ww.CreateItem(ui, element, name); }
         };
 
         let basic = new ww.Basic();
