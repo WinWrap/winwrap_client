0fa39d284843bf6133cb1852d9cba99eda16a3b1
EdwBennett <edw.bennett@gmail.comx>
2018-02-06 04:01:58 -0600

    hover

--- winwrap/debugdecorate.js
+++ winwrap/debugdecorate.js
@@ -41,7 +41,7 @@
                 let line = syntaxerror.line_num;
                 let decoration = {};
                 //decoration.range = new monaco.Range(line, 1, line, 1);
-                decoration.range = new monaco.Range(3, 1, 3, 10);
+                //decoration.range = new monaco.Range(3, 1, 3, 10);
                 //decoration.options = { isWholeLine: true, className: 'myErrorClass' };
                 /*decoration.options = {
                     className: 'myContentClass',
@@ -50,7 +50,6 @@
                 };*/
                 decoration.options = {
                     className: 'myContentClass',
-                    glyphMarginClassName: 'myDebugPauseClass',
                     hoverMessage: 'hover message'
                 };
                 decorations.push(decoration);
