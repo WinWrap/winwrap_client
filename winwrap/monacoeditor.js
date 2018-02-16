43809421cd402fef0018d0747bd9df625b7947e3
EdwBennett <edw.bennett@gmail.comx>
2018-02-16 11:19:00 -0600

    try

--- winwrap/monacoeditor.js
+++ winwrap/monacoeditor.js
@@ -80,7 +80,7 @@
                 console.log(`this.element_.innerHeight() = ${this.element_.innerHeight()}`)
                 let elMenu = $("#ww-remote-1-menu");
                 let elVersion = $("#ww-remote-1-version");
-                let elMenuBottom = elMenu.position().top - elMenu.height();
+                let elMenuBottom = (elMenu.position()).top - elMenu.height();
                 let elVersionheight = elVersion.height();
                 let height = $(window).height() - elMenuBottom - elVersionHeight;
                 //let position = uibuttons.position();
