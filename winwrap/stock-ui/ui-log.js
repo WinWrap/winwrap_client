0168ac6b4054e7e78282fb2b341d50ef89563947
Tom Bennett <tom.bennett@winwrap.com>
2018-03-30 12:02:36 -0800

    better json logging

--- winwrap/stock-ui/ui-log.js
+++ winwrap/stock-ui/ui-log.js
@@ -9,45 +9,16 @@
 // Copyright 2017-2018 Polar Engineering, Inc.
 // All rights reserved.
 
-define(function () {
+define(['./renderjson'], function () {
 
     class Log {
         constructor(ui, channel, element) {
             this.element_ = element;
-            let list = $('<ul/>');
-            list.appendTo(element);
+            renderjson.set_show_to_level(1);
             let this_ = this; // closure can't handle this in the lambdas below
             channel.SetLogger((label, data) => {
-                let html = this_._Html(label, data);
-                html.appendTo(list);
+                element.append(renderjson(data));
             });
-        }
-
-        _Html(label, data) {
-            let item = $('<li/>');
-            if (Array.isArray(data)) {
-                item.text(label);
-                let list = $('<ul/>');
-                let i = 0;
-                data.forEach(value => {
-                    this._Html(`[${i}] `, value).appendTo(list);
-                    ++i;
-                });
-                list.appendTo(item);
-            }
-            else if (data !== null && typeof data === 'object') {
-                item.text(label);
-                let list = $('<ul/>');
-                Object.keys(data).forEach(key => {
-                    let value = data[key];
-                    this._Html(key, value).appendTo(list);
-                });
-                list.appendTo(item);
-            }
-            else {
-                item.text(label +': ' + JSON.stringify(data));
-            }
-            return item;
         }
     }
 
