8589c6c99b8bff42d2b8bf731961d1ad7947fec9
Edw <WinWrap@users.noreply.github.com>
2018-04-09 14:49:24 -0500

    fix

--- winwrap/stock-ui/ui-files.js
+++ winwrap/stock-ui/ui-files.js
@@ -23,7 +23,7 @@ define(['./ui'], function () {
                 detached: response => {
                     if (response.detached_id === 0 || response.detached_id === channel.AllocatedID) {
                         // disable the input box
-                        this.element.autocomplete('disable');
+                        this.element_.autocomplete('disable');
                     }
                 },
                 new: response => {
